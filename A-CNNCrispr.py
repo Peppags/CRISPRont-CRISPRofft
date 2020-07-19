92e500f7007fcadb757b0a156694d39855a407a9
Sunflower <40748992+Peppags@users.noreply.github.com>
2020-07-19 23:03:20 +0800

    Update A-CNNCrispr.py

--- A-CNNCrispr.py
+++ A-CNNCrispr.py
@@ -1,23 +1,19 @@
 # -*- coding:utf-8 -*-
-
 import os
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
-import tensorflow as tf
-import keras.backend.tensorflow_backend as KTF
-
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.5
-session = tf.Session(config=config)
-KTF.set_session(session)
 
 from keras.preprocessing import text, sequence
 from keras.models import Model
-from keras.layers import Input, Embedding, Lambda, BatchNormalization
-from keras.layers.convolutional import Conv1D
-from keras.layers.core import Dense, Dropout, Flatten
-from keras.optimizers import *
-import numpy as np
+from keras.layers import Input, Embedding
+from keras.layers.core import Dropout
+from keras.layers.convolutional import Conv1D, AveragePooling1D
+from keras.layers import multiply
+from keras.layers.core import Dense, Reshape, Lambda, Permute, Flatten
+from keras.initializers import RandomUniform
+from keras import regularizers
+import keras.backend as K
+import keras
 import pandas as pd
 
 
@@ -28,8 +24,7 @@ def make_data(X):
     char_dict = {}
     for i, char in enumerate(alphabet):
         char_dict[char] = i + 1
-
-    word_index = {k: (v+1) for k, v in char_dict.items()}
+    word_index = {k: (v + 1) for k, v in char_dict.items()}
     word_index["PAD"] = 0
     word_index["START"] = 1
     vectorizer.word_index = word_index.copy()
@@ -39,72 +34,77 @@ def make_data(X):
     return X
 
 
-def AttentionLayer(inputs):
-    time_steps = inputs.shape[1].value
-    W = K.random_uniform_variable(shape=(time_steps, time_steps), low=0, high=1)
-    b = K.random_uniform_variable(shape=(time_steps, ), low=0, high=1)
-    x = K.permute_dimensions(inputs, (0, 2, 1))
-    x1 = Lambda(lambda x: K.dot(x, W) + b)(x)
-    x2 = Lambda(lambda x: K.tanh(x))(x1)
-    a = K.softmax(x2)
-    outputs = Lambda(lambda x: x[0] * x[1])([a, x])
-    outputs = K.permute_dimensions(outputs, (0, 2, 1))
-    return outputs
+def attention(x, g, TIME_STEPS):
+    input_dim = int(x.shape[2])
+    x1 = K.permute_dimensions(x, (0, 2, 1))
+    g1 = K.permute_dimensions(g, (0, 2, 1))
+
+    x2 = Reshape((input_dim, TIME_STEPS))(x1)
+    g2 = Reshape((input_dim, TIME_STEPS))(g1)
+    x3 = Dense(TIME_STEPS, kernel_initializer=RandomUniform(seed=2020))(x2)
+    g3 = Dense(TIME_STEPS, kernel_initializer=RandomUniform(seed=2020))(g2)
+    x4 = keras.layers.add([x3, g3])
+    a = Dense(TIME_STEPS, activation='softmax', use_bias=False)(x4)
+    a_probs = Permute((2, 1))(a)
+    output_attention_mul = multiply([x, a_probs])
+    return output_attention_mul
 
 
 def main():
-    dropout_rate = 0.3
+    dropout_rate = 0.4
     input = Input(shape=(24,))
     embedded = Embedding(7, 44, input_length=24)(input)
 
     conv1 = Conv1D(256, 5, activation='relu', name="conv1")(embedded)
-    batchnor1 = BatchNormalization()(conv1)
-    atten1 = Lambda(lambda x: AttentionLayer(x))(batchnor1)
-    drop1 = Dropout(dropout_rate)(atten1)
+    pool1 = AveragePooling1D(2)(conv1)
+    drop1 = Dropout(dropout_rate)(pool1)
 
-    conv2 = Conv1D(128, 5, activation='relu', name="conv2")(drop1)
-    batchnor2 = BatchNormalization()(conv2)
-    atten2 = Lambda(lambda x: AttentionLayer(x))(batchnor2)
-    drop2 = Dropout(dropout_rate)(atten2)
+    conv2 = Conv1D(256, 5, activation='relu', name="conv2")(pool1)
 
-    flat = Flatten()(drop2)
+    conv11 = Conv1D(256, 5, activation='relu', name="conv11")(drop1)
 
-    dense1 = Dense(128, activation='relu', name="dense1")(flat)
-    batchnor3 = BatchNormalization()(dense1)
-    drop3 = Dropout(dropout_rate)(batchnor3)
+    x = Lambda(lambda x: attention(x[0], x[1], 6))([conv11, conv2])
 
-    dense2 = Dense(64, activation='relu', name="dense2")(drop3)
-    batchnor4 = BatchNormalization()(dense2)
-    drop4 = Dropout(dropout_rate)(batchnor4)
+    my_concat = Lambda(lambda x: K.concatenate([x[0], x[1]], axis=1))
+    weight_1 = Lambda(lambda x: x * 0.2)
+    weight_2 = Lambda(lambda x: x * 0.8)
+
+    flat1 = Flatten()(pool1)
+    flat2 = Flatten()(x)
+    flat = my_concat([weight_1(flat1), weight_2(flat2)])
+
+    dense1 = Dense(128,
+                   kernel_regularizer=regularizers.l2(1e-4),
+                   bias_regularizer=regularizers.l2(1e-4),
+                   activation='relu',
+                   name="dense1")(flat)
+    drop3 = Dropout(dropout_rate)(dense1)
+
+    dense2 = Dense(64,
+                   kernel_regularizer=regularizers.l2(1e-4),
+                   bias_regularizer=regularizers.l2(1e-4),
+                   activation='relu',
+                   name="dense2")(drop3)
+    drop4 = Dropout(dropout_rate)(dense2)
 
     dense3 = Dense(32, activation='relu', name="dense3")(drop4)
-    batchnor5 = BatchNormalization()(dense3)
-    drop5 = Dropout(dropout_rate)(batchnor5)
+    drop5 = Dropout(dropout_rate)(dense3)
 
     output = Dense(1, activation='linear', name="output")(drop5)
 
     model = Model(inputs=[input], outputs=[output])
 
     print("Loading weights for the models")
-    model.load_weights('weights/A_CNNCrispr_weights.h5')
+    model.load_weights('weights/weights.h5')
+
+    test_file = "data/test.csv"
 
     print("Loading test data")
-    test_file = "data/test_data.csv"
-    data = pd.read_csv(test_file, usecols=[0, 1])
-    data = np.array(data)
-    x_data, y_test = data[:, 0], data[:, 1]
-
-    x_test = make_data(x_data)
-    y_test = y_test.reshape(len(y_test), -1)
+    data = pd.read_csv(test_file)
+    x_test = make_data(data['sgRNA'])
 
     print("Predicting on test data")
-    result_file = "result/A-CNNCrispr_result.csv"
-    y_test = pd.DataFrame(y_test)
-    y_pred = model.predict([x_test], batch_size=256, verbose=2)
-    y_pred = pd.DataFrame(y_pred)
-
-    result = pd.concat([y_test, y_pred], axis=1)
-    result.to_csv(result_file, index=False, sep=',', header=['y_test', 'y_pred'])
+    y_pred = model.predict([x_test], batch_size=128, verbose=2)
 
 
 if __name__ == '__main__':
