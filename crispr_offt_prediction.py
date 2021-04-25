85a0b7b4d5bde6935488905ae0cc48ea9d48144d
Sunflower <40748992+Peppags@users.noreply.github.com>
2021-04-25 10:28:35 +0800

    Update crispr_offt_prediction.py

--- crispr_offt_prediction.py
+++ crispr_offt_prediction.py
@@ -36,17 +36,6 @@ def attention(x, g, TIME_STEPS):
     return output_attention_mul
 
 
-def loadData(data_file):
-    data_list, data = [], []
-    with open(data_file) as f:
-        for line in f:
-            l = [i for i in line.strip().split(',')]
-            data_item = [int(i) for i in l[2:]]
-            data.append(l)
-            data_list.append(data_item)
-    return data
-
-
 VOCAB_SIZE = 16
 EMBED_SIZE = 90
 MAXLEN = 23
@@ -85,68 +74,10 @@ if __name__ == '__main__':
 
     print("Loading weights for the models")
     model.load_weights("crispr_offt.h5")
+    encoded_file = "data/encoded_test_offt.txt"
+        
+    xtest = np.array(loadData(encoded_file))
+    print("Predicting on test data")
+    y_pred = model.predict(xtest[:, 2:])
 
-    sgrna = input("\nInput the sgRNA sequence followed by the PAM sequence(23 base pair sequence):\n")
-    dna = input("\nInput the DNA sequence:\n")
-    proceed_flag = 1
-
-    if len(sgrna) < 23 or len(dna) < 23:
-        print("Sequence is too short.")
-        proceed_flag = 0
-
-    if sgrna.count('A') + sgrna.count('T') + sgrna.count('C') + sgrna.count('G') != len(sgrna):
-        print("The sgRNA sequence should contains four characters A, T, C and G.")
-        proceed_flag = 0
-
-    if dna.count('A') + dna.count('T') + dna.count('C') + dna.count('G') != len(dna):
-        print("DNA sequence should contains four characters A, T, C and G.")
-        proceed_flag = 0
-
-    if len(sgrna) > 23 or len(dna) > 23:
-        print("Sequence is too long.")
-        proceed_flag = 0
-
-    if proceed_flag == 1:
-        sgrna = pd.DataFrame(np.array(sgrna).reshape(-1))
-        dna = pd.DataFrame(np.array(dna).reshape(-1))
-        offt_data = pd.concat([sgrna, dna], axis=1)
-
-        offt_data_path = "data/test_offt.csv"
-        offt_data.to_csv(offt_data_path, index=False, sep=',', header=['sgRNA', 'DNA'])
-
-        offt_data = pd.read_csv(offt_data_path)
-
-        match_dic = {
-                      "AA": 1, "AC": 2, "AG": 3, "AT": 4,
-                      "CA": 5, "CC": 6, "CG": 7, "CT": 8,
-                      "GA": 9, "GC": 10, "GG": 11, "GT": 12,
-                      "TA": 13, "TC": 14, "TG": 15, "TT": 16
-                    }
-
-        sgrna_list = offt_data['sgRNA'].values
-        dna_list = offt_data['DNA'].values
-
-        encoded_file = "data/encoded_test_offt.txt"
-        encoded_data = open(encoded_file, 'w')
-        length = len(offt_data)
-        for i in range(length):
-            vector = []
-            vector.append(sgrna_list[i])
-            vector.append(dna_list[i])
-            for j in range(len(sgrna_list[i])):
-                temp = sgrna_list[i][j] + dna_list[i][j]
-                vector.append(match_dic[temp] - 1)
-            encoded_data.writelines(",".join('%s' % item for item in vector) + '\n')
-        encoded_data.close()
-
-        xtest = loadData(encoded_file)
-        xtest = np.array(xtest)
-
-        print("Predicting on test data")
-        y_pred = model.predict(xtest[:, 2:])
-        max_index = np.argmax(y_pred)
-        if max_index == 0:
-            print('The input sequence belongs to non-off-target with possibility %.4f' % y_pred[:, 0])
-        else:
-            print('The input sequence belongs to off-target with possibility %.4f' % y_pred[:, 1])
 
